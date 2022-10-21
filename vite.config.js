3b83d7a82d8c34ec4e96f83338c8302df9000c90
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2022-10-21 08:46:18 -0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/Todolist_vue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
